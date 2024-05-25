1f4e30684d61b3be5c591cdd45f635be3c8d3bfd
anhvy0904 <anhvydn2005@gmail.com>
2024-05-25 08:51:07 +0700

    add

--- src/main/webapp/assets/js/index.js
+++ src/main/webapp/assets/js/index.js
@@ -329,3 +329,20 @@ if(loginStatus == "true") {
 window.addEventListener('load',function(){
     loadingFrame.style.display = "none";
 })
+
+//quote
+document.addEventListener('DOMContentLoaded', function() {
+    const quote = document.getElementById("quote");
+    const author = document.getElementById("author");
+    const api_url = "https://api.quotable.io/random";
+
+    async function getQuote(url){
+        const response = await fetch(url);
+        var data = await response.json();
+
+        quote.innerHTML = data.content;
+        author.innerHTML = data.author;
+    }
+
+    getQuote(api_url);
+});
